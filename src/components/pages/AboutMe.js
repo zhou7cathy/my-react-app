edb68eb1f2e3f5772cbbb28fc5a23202bd37b20c
unknown <zhou7cathy@gmail.com>
2023-04-17 21:50:38 +0800

    updated contact me page.

--- src/components/pages/AboutMe.js
+++ src/components/pages/AboutMe.js
@@ -5,12 +5,9 @@ export default function AboutMe() {
     <div>
       <h1>About Me</h1>
       <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
+        Hello, my name is Yanfang Zhou. I am learning to become a full stack developer. 
+        I am trained at UWA coding boot camp and earned a certificate in full-stack web development.
+        This portfolio content my works through out the course. 
       </p>
     </div>
   )
