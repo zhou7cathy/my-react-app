258e0d2d89c27cd59e5ac280a3550b84cd8333a5
unknown <zhou7cathy@gmail.com>
2023-04-19 12:47:31 +0800

    updated contact page and created helper functions to check validation of email and message.

--- src/components/pages/Contact.js
+++ src/components/pages/Contact.js
@@ -1,17 +1,91 @@
-import React from 'react'
+import React,{ useState } from 'react'
+import '../style.css';
 
-export default function Contact() {
+//import a helper function that will check if the email and message is valid
+import { validateEmail, checkMessage } from '../../utils/helpers';
+
+function Form() {
+  // Create state variables for the fields in the form
+  const [name, setName] = useState('');
+  const [email, setEmail] = useState('');
+  const [message, setMessage] = useState('');
+  const [errorMessage, setErrorMessage] = useState('');
+
+  const handleInputChange = (e) => {
+    // Getting the value and name of the input which triggered the change
+    const { target } = e;
+    const inputType = target.name;
+    const inputValue = target.value;
+
+    // Based on the input type, we set the state of either name, email and message
+    if (inputType === 'name') {
+      setName(inputValue);
+    } else if (inputType === 'email') {
+      setEmail(inputValue);
+    } else {
+      setMessage(inputValue);
+    }
+  };
+
+  const handleFormSubmit = (e) => {
+    // Preventing the default behavior of the form submit (which is to refresh the page)
+    e.preventDefault();
+
+    // Check to see if the email is not valid 
+    if (!validateEmail(email)) {
+      setErrorMessage('Email is invalid.');
+      return;
+    }
+    // Check to see if the message is not valid
+    if (!checkMessage(message)) {
+      setErrorMessage(
+        `Message is required.`
+      );
+      return;
+    }
+
+    // If everything goes according to plan, we want to clear out the input after a successful registration.
+    setName('');
+    setEmail('');
+    setMessage('');
+    setErrorMessage('');
+  };
+
   return (
     <div>
-      <h1>Contact Me</h1>
-      <p>
-        Nunc pharetra finibus est at efficitur. Praesent sed congue diam.
-        Integer gravida dui mauris, ut interdum nunc egestas sed. Aenean sed
-        mollis diam. Nunc aliquet risus ac finibus porta. Nam quis arcu non
-        lectus tincidunt fermentum. Suspendisse aliquet orci porta quam semper
-        imperdiet. Praesent euismod mi justo, faucibus scelerisque risus cursus
-        in. Sed rhoncus mollis diam, sit amet facilisis lectus blandit at.
-      </p>
+      <form className="form">
+        <label>Name:</label>
+        <input
+          value={name}
+          name="name"
+          onChange={handleInputChange}
+          type="text"
+        />
+        <label>Email:</label>
+        <input
+          value={email}
+          name="email"
+          onChange={handleInputChange}
+          type="email"
+        />
+        <label>Message:</label>
+        <input
+          value={message}
+          name="message"
+          onChange={handleInputChange}
+          type="message"
+        />
+        <button type="button" onClick={handleFormSubmit}>
+          Submit
+        </button>
+      </form>
+      {errorMessage && (
+        <div>
+          <p className="error-text">{errorMessage}</p>
+        </div>
+      )}
     </div>
-  )
+  );
 }
+
+export default Form;
---
+++ src/utils/helpers.js
@@ -0,0 +1,9 @@
+export function validateEmail(email) {
+  const re = /^[\w-\.]+@([\w-]+\.)+[\w-]{2,4}$/;
+  return re.test(String(email).toLowerCase());
+}
+
+export function checkMessage(message) {
+  const msg = /\w/;
+  return msg.test(message);
+}
